db71c085fd6c3ac4a83821e25c36edde2234281a
Mai Thành Duy An <tiachop0102@gmail.com>
2023-10-04 00:42:24 +0900

    [Java-CLI] client ui

--- java-cli/client/src/TCPClient.java
+++ java-cli/client/src/TCPClient.java
@@ -1,31 +1,54 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class TCPClient {
 	public static void main(String[] args) {
-		String serverAddress = "localhost"; // Địa chỉ IP hoặc tên máy chủ của server
+		String serverAddress = "10.220.56.18"; // Địa chỉ IP hoặc tên máy chủ của server
 		int port = 3000; // Số cổng của server
 		User player1 = new User("playertest1", "123456");
+		String token = null;
 		try {
 			Socket socket = new Socket(serverAddress, port);
-			System.out.println("Connected to server: " + socket.getInetAddress().getHostAddress());
+			AsynLogger.logInfo("Connected to server: " + socket.getInetAddress().getHostAddress());
 
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
 			// Gửi tin nhắn tới server
-			out.println(player1.toString()+"\tab"+"Deal");
+			out.println("LOGIN:" + player1.toString());
 
 			// Đọc phản hồi từ server
 			String response = in.readLine();
-			System.out.println("Server response: " + response);
-
+			AsynLogger.logInfo("Server response: " + response);
+			boolean serverAccepted = false;
+			if (response.startsWith("TOKEN:")) {
+				token = response.substring(7);
+				serverAccepted = true;
+				while (serverAccepted) {
+					// gửi yêu cầu tiếp theo
+					//out.println("TOKEN:" + token);
+					Scanner scanner = new Scanner(System.in);
+					System.out.print("Nhập: ");
+					String option = scanner.nextLine();
+					
+					// nhận thông tin
+					AsynLogger.logInfo(in.readLine());
+					
+					
+					try {
+						Thread.sleep(200);
+					} catch (InterruptedException e) {
+						AsynLogger.logInfo("InterruptedException: " + e.getMessage());
+					}
+				}
+			}
 			// Đóng kết nối
 			in.close();
 			out.close();
 			socket.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			AsynLogger.logInfo("Lỗi:" + e.getMessage());
 		}
 	}
 }
