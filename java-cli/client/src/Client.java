c6372e4b35c5775d961d6f3a81fd1ac915bb1399
Mai Thành Duy An <tiachop0102@gmail.com>
2023-10-04 13:32:08 +0900

    [Java-CLI] Client refactoring.

--- java-cli/client/src/Client.java
+++ java-cli/client/src/Client.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-public class TCPClient {
+public class Client {
 	public static void main(String[] args) {
 		String serverAddress = "10.220.56.18"; // Địa chỉ IP hoặc tên máy chủ của server
 		int port = 3000; // Số cổng của server
@@ -22,28 +22,9 @@ public class TCPClient {
 			String response = in.readLine();
 			AsynLogger.logInfo("Server response: " + response);
 			boolean serverAccepted = false;
-			if (response.startsWith("TOKEN:")) {
-				token = response.substring(7);
-				serverAccepted = true;
-				while (serverAccepted) {
-					// gửi yêu cầu tiếp theo
-					//out.println("TOKEN:" + token);
-					Scanner scanner = new Scanner(System.in);
-					System.out.print("Nhập: ");
-					String option = scanner.nextLine();
-					
-					// nhận thông tin
-					AsynLogger.logInfo(in.readLine());
-					
-					
-					try {
-						Thread.sleep(200);
-					} catch (InterruptedException e) {
-						AsynLogger.logInfo("InterruptedException: " + e.getMessage());
-					}
-				}
-			}
+			
 			// Đóng kết nối
+			System.out.println("Đóng kết nối");
 			in.close();
 			out.close();
 			socket.close();
