b40ebcfdd7339731060b05a8d2442eaaf517e0a7
Mai Thành Duy An <tiachop0102@gmail.com>
2023-10-05 01:16:39 +0900

    [Java-CLI] Client chat to server

--- java-cli/client/src/main/java/Client.java
+++ java-cli/client/src/main/java/Client.java
@@ -6,36 +6,82 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Client {
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(Client.class);
-	
+	static final int PORT = 3000; // Số cổng của server
+	static boolean isConnecting = false;
+
 	public static void main(String[] args) {
-		String serverAddress = "10.220.56.18"; // Địa chỉ IP hoặc tên máy chủ của server
-		int port = 3000; // Số cổng của server
+
+		// Địa chỉ IP hoặc tên máy chủ của server
+		String serverAddress = "10.220.56.18";
+
 		User player1 = new User("playertest1", "123456");
-		String token = null;
+
+		Scanner scn = new Scanner(System.in);
+
+		// getting localhost ip
+		InetAddress ip = null;
+
+		// establish the connection
+		Socket s;
+
 		try {
-			Socket socket = new Socket(serverAddress, port);
-			LOG.info("Connected to server: " + socket.getInetAddress().getHostAddress());
+			ip = InetAddress.getByName(serverAddress);
+			s = new Socket(ip, PORT);
+			// obtaining input and out streams
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			isConnecting = true;
+			// sendMessage thread
+			Thread sendMessage = new Thread(new Runnable() {
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				@Override
+				public void run() {
+					LOG.info("Tạo luồng gửi data.");
+					while (isConnecting) {
 
-			// Gửi tin nhắn tới server
-			out.println("LOGIN:" + player1.toString());
+						// read the message to deliver.
+						String msg = scn.nextLine();
 
-			// Đọc phản hồi từ server
-			String response = in.readLine();
-			LOG.info("Server response: " + response);
-			boolean serverAccepted = false;
-			
-			// Đóng kết nối
-			LOG.info("Đóng kết nối");
-			in.close();
-			out.close();
-			socket.close();
+						try {
+							// write on the output stream
+							dos.writeUTF(msg);
+						} catch (IOException e) {
+							LOG.error("Lỗi Gửi Data: " + e.getMessage());
+							isConnecting = false;
+							break;
+						}
+					}
+					//
+				}
+			});
+
+			// readMessage thread
+			Thread readMessage = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					LOG.info("Tạo luồng nhận data.");
+					while (isConnecting) {
+						try {
+							// read the message sent to this client
+							String msg = dis.readUTF();
+							System.out.println("Server:" + msg);
+						} catch (Exception e) {
+							LOG.error("Lỗi Nhận Data:" + e.getMessage());
+							isConnecting = false;
+							break;
+						}
+					}
+				}
+			});
+
+			sendMessage.start();
+			readMessage.start();
+
 		} catch (IOException e) {
-			LOG.error("Lỗi:" + e.getMessage());
+			LOG.error("Lỗi: " + e.getMessage());
 		}
+
 	}
 }
