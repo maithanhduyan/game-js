7150fda3565a32c3ea1b42b15b882fccfc7f290a
Mai Thành Duy An <tiachop0102@gmail.com>
2023-10-09 01:04:14 +0900

    [Java] Multi websocket onsite

---
+++ java/WebsocketHome/src/main/java/websocket/echo/EchoEndpoint.java
@@ -0,0 +1,62 @@
+package websocket.echo;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import jakarta.websocket.Endpoint;
+import jakarta.websocket.EndpointConfig;
+import jakarta.websocket.MessageHandler;
+import jakarta.websocket.RemoteEndpoint;
+import jakarta.websocket.Session;
+
+public class EchoEndpoint extends Endpoint {
+
+	@Override
+	public void onOpen(Session session, EndpointConfig endpointConfig) {
+		RemoteEndpoint.Basic remoteEndpointBasic = session.getBasicRemote();
+		session.addMessageHandler(new EchoMessageHandlerText(remoteEndpointBasic));
+		session.addMessageHandler(new EchoMessageHandlerBinary(remoteEndpointBasic));
+	}
+
+	private static class EchoMessageHandlerText implements MessageHandler.Partial<String> {
+
+		private final RemoteEndpoint.Basic remoteEndpointBasic;
+
+		private EchoMessageHandlerText(RemoteEndpoint.Basic remoteEndpointBasic) {
+			this.remoteEndpointBasic = remoteEndpointBasic;
+		}
+
+		@Override
+		public void onMessage(String message, boolean last) {
+			try {
+				if (remoteEndpointBasic != null) {
+					remoteEndpointBasic.sendText(message, last);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static class EchoMessageHandlerBinary implements MessageHandler.Partial<ByteBuffer> {
+
+		private final RemoteEndpoint.Basic remoteEndpointBasic;
+
+		private EchoMessageHandlerBinary(RemoteEndpoint.Basic remoteEndpointBasic) {
+			this.remoteEndpointBasic = remoteEndpointBasic;
+		}
+
+		@Override
+		public void onMessage(ByteBuffer message, boolean last) {
+			try {
+				if (remoteEndpointBasic != null) {
+					remoteEndpointBasic.sendBinary(message, last);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
